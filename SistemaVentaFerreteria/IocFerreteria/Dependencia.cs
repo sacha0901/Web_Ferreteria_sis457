8b87e6ea84233a8a1f35b2822d88b9621be52fbe
sacha0901 <asoigo0901@gmail.com>
2023-06-17 02:01:38 -0400

    first commit

---
+++ SistemaVentaFerreteria/IocFerreteria/Dependencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CadFerreteria.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+//using CadFerreteria.Interfaces;
+//using CadFerreteria.Implementacion;
+//using ClnFerreteria.Interfaces;
+//using CnlFerreteria.Implementacion;
+
+namespace IocFerreteria
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
